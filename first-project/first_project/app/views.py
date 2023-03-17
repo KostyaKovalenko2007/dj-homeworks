e0057ed2d7564c2740400810afa9eb2861124dd2
kkovalenko <kovalenkokv@gmail.com>
2023-03-17 16:04:10 +0300

    первое ДЦ по Джанго

--- first-project/first_project/app/views.py
+++ first-project/first_project/app/views.py
@@ -1,5 +1,8 @@
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+from datetime import datetime
 
 
 def home_view(request):
@@ -8,28 +11,61 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
     context = {
-        'pages': pages
+        'pages': pages,
+        'message':[]
     }
     return render(request, template_name, context)
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
-    current_time = None
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    current_time = datetime.utcnow()
     msg = f'Текущее время: {current_time}'
-    return HttpResponse(msg)
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages,
+        'message': [msg]
+    }
+    # обратите внимание – здесь HTML шаблона нет,
+    # возвращается просто текст
+
+    return render(request, template_name, context)
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
-    raise NotImplemented
+    dir_list = os.listdir()
+    msg = [item for item in os.listdir() ]
+    msg.insert(0,'Содержимое рабочей директории:')
+    print(msg)
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    context = {
+        'pages': pages,
+        'message': msg
+    }
+    # обратите внимание – здесь HTML шаблона нет,
+    # возвращается просто текст
+
+    return render(request, template_name, context)
+    #return HttpResponse(msg)
